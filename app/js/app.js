309911a04985c03863cfa8a8fd399a968237bf9d
smngrvn <simon.grevin@gmail.com>
2016-08-05 23:01:08 +0200

    ajout de firebase

--- app/js/app.js
+++ app/js/app.js
@@ -31,3 +31,19 @@ router.map({
 
 // démarrage de l'app
 router.start(App, '#app')
+
+
+
+// initialisation de firebase
+let config = {
+  apiKey: "AIzaSyA8JaAXms67CPMBFn17V6soZP1vLxKbqVM",
+  authDomain: "projet-gpo.firebaseapp.com",
+  databaseURL: "https://projet-gpo.firebaseio.com",
+  storageBucket: "projet-gpo.appspot.com",
+};
+firebase.initializeApp(config);
+//firebase.database().ref('montures/').push({
+//  marque: "Rayban",
+//  prix_affiche: 300,
+//  prix_minimum : 200
+//});
