1ac951180a0802f264e84317ba7eb116a38b93be
smngrvn <simon.grevin@gmail.com>
2016-08-05 21:49:19 +0200

    ajout de vue router

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 'use strict'
 
-let app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Bienvenue sur GPO'
-  }
-});
+// vue des clients
+var ClientVue = Vue.extend({
+    template: '<div id="clients">Liste des clients</p>'
+})
+
+// vue des montures
+var MontureVue = Vue.extend({
+    template: '<div id="montures">Liste des montures</p>'
+})
+
+// Vue App
+var App = Vue.extend({})
+
+// Vue Router
+var router = new VueRouter()
+
+// routes
+router.map({
+    '/': {
+        component: Vue.extend({template: '<div id="accueil">Bienvenue sur GPO</div>'})
+    },
+    '/clients': {
+        component: ClientVue
+    },
+    '/stock': {
+        component: MontureVue
+    }
+})
+
+// démarrage de l'app
+router.start(App, '#app')
