f95ffeee589bf53820b3830558ddc38ce92c4a5e
smngrvn <simon.grevin@gmail.com>
2016-08-09 22:16:55 +0200

    export de la vue des montures dans son propre fichier/module

---
+++ app/js/montures.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let MODULE_MONTURES = (function () {
+
+  let module = {}
+
+  // vue des montures
+  module.MontureVue = Vue.extend({
+      template: '<div id="montures">Liste des montures</p>'
+  })
+
+	return module;
+
+}());
